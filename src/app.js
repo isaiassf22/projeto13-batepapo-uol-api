7c1599d49761544ccc87a92d256ca9b09f6f7031
Isaias <figueiredo.isaias@gmail.com>
2023-01-13 15:09:45 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
+import dayjs from "dayjs"
 
-
+dayjs().format()
 const app= express()
 app.use(cors())
 app.use(express.json())
 
+const participants= [{name: 'xxx1', lastStatus: dayjs()},{name: 'xxx3', lastStatus: dayjs()},{name: 'xxx2', lastStatus: dayjs()}]
+
+
 app.get("/", (req,res)=>{
-    res.send("Hello world!!")
+    res.send(participants)
 })
 
-h
+
 
 app.listen(5000, ()=> console.log("app rodando"))
