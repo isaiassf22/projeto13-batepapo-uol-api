65439ab9ffce59f33e4f0e372a9a84ec1462afae
Isaias <figueiredo.isaias@gmail.com>
2023-01-15 22:00:52 -0300

    get and post of the participants with verification

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app= express()
 app.use(cors())
 app.use(express.json())
 
-const mongoClient = new MongoClient(process.env.MONGO_URI)
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
 
 const lastStatus=dayjs().format("HH:MM:SS")
 
@@ -30,24 +30,46 @@ const db = mongoClient.db("batepapoUol")
 const userCollection=db.collection("users")
 
 
-app.get("/participants", (req,res)=>{
-    
+app.get("/participants", async (req,res)=>{
+    try{
+      const users = await userCollection.find().toArray()
+        if(!users){
+            return res.status(404)
+        }
+        res.send(users)
+    }catch(err) {
+        console.log(err)
+        res.status(500)
+    }
 })
 
-app.post("/participants", (req,res)=>{ //cadastro
+app.post("/participants", async (req,res)=>{ //cadastro
 const {name}=req.body
-//userCollection.find({name})
+ const {error, value}= userSchema.validate({name})
+ if(error){
+    return res.status(422).send("usuario não cadastrado! Coloque um nome valido")
+ }
+ try{
+    const userExistent = await userCollection.findOne({name});
+    if (userExistent){
+        return res.status(409).send("Esse usuario já exite!");
+    }
+    await userCollection.insertOne({name, lastStatus:Date.now()})
+    res.status(201).send("Usuario cadastrado com sucesso!")
+
+ }catch{
+
+ }
 })
 
 
-app.post("/messages", (req,res)=>{
+app.post("/messages",async (req,res)=>{
     
 })
 
-app.get("/messages", (req,res)=>{
+app.get("/messages",async (req,res)=>{
  
 })
-
 
 
 app.listen(5000, ()=> console.log("app rodando"))
