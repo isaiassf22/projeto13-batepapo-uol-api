26ee0c77323c23cf4c0568e3b141a918db0559fa
Isaias <figueiredo.isaias@gmail.com>
2023-01-17 13:25:13 -0300

    interval to inactive people

--- src/app.js
+++ src/app.js
@@ -6,73 +6,73 @@ import dayjs from "dayjs"
 import Joi from "joi"
 dotenv.config()
 
-const app= express()
+const app = express()
 app.use(cors())
 app.use(express.json())
 
 const mongoClient = new MongoClient(process.env.DATABASE_URL)
 
-const lastStatus=dayjs().format("HH:mm:ss")
+const lastStatus = dayjs().format("HH:mm:ss")
 
 
 
-const userShema=Joi.object({
-    name:Joi.string().min(3).max(30).required()  
+const userShema = Joi.object({
+    name: Joi.string().min(3).max(30).required()
 })
 
 const messageShema = Joi.object({
-    to:Joi.string().min(3).max(30).required(),
-    text:Joi.string().min(1).required(),
-    type:Joi.string().min(1)
+    to: Joi.string().min(3).max(30).required(),
+    text: Joi.string().min(1).required(),
+    type: Joi.string().min(1)
 })
 
 try {
     await mongoClient.connect();
     console.log(" Mongodb conectado com sucesso!")
-}catch(err){
+} catch (err) {
     console.log(err)
 }
 const db = mongoClient.db("batepapoUol")
-const userCollection=db.collection("users")
-const messagesCollection= db.collection("menssages")
+const userCollection = db.collection("users")
+const messagesCollection = db.collection("menssages")
 
 
-app.post("/participants", async (req,res)=>{ //cadastro
-const {name}=req.body
- const {error, value}= userShema.validate({name})
- if(error){
-    return res.status(422).send("usuario não cadastrado! Coloque um nome valido")
- }
- try{
-    const userExistent = await userCollection.findOne({name});
-    if (userExistent){
-        return res.status(409).send("Esse usuario já exite!");
+app.post("/participants", async (req, res) => { //cadastro
+    const { name } = req.body
+    const { error, value } = userShema.validate({ name })
+    if (error) {
+        return res.status(422).send("usuario não cadastrado! Coloque um nome valido")
     }
-    await userCollection.insertOne({name, lastStatus:Date.now()})
-    
-    await messagesCollection.insertOne({
-        from: name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status",
-        time:lastStatus,
-      });
-    res.status(201).send("Usuario cadastrado com sucesso!")
+    try {
+        const userExistent = await userCollection.findOne({ name });
+        if (userExistent) {
+            return res.status(409).send("Esse usuario já exite!");
+        }
+        await userCollection.insertOne({ name, lastStatus: Date.now() })
 
- }catch(err){
-    console.log(err)
- }
+        await messagesCollection.insertOne({
+            from: name,
+            to: "Todos",
+            text: "entra na sala...",
+            type: "status",
+            time: lastStatus,
+        });
+        res.status(201).send("Usuario cadastrado com sucesso!")
+
+    } catch (err) {
+        console.log(err)
+    }
 })
 
 
-app.get("/participants", async (req,res)=>{
-    try{
-      const users = await userCollection.find().toArray()
-        if(!users){
+app.get("/participants", async (req, res) => {
+    try {
+        const users = await userCollection.find().toArray()
+        if (!users) {
             return res.status(404)
         }
         res.send(users)
-    }catch(err) {
+    } catch (err) {
         console.log(err)
         res.status(500)
     }
@@ -80,96 +80,106 @@ app.get("/participants", async (req,res)=>{
 
 
 
-app.post("/messages",async (req,res)=>{
-    const {to,text,type}= req.body
-    const {user}=req.headers
-    const {error,value}=messageShema.validate({to:to,text:text,type:type})
-    if(error){
+app.post("/messages", async (req, res) => {
+    const { to, text, type } = req.body
+    const { user } = req.headers
+    const { error, value } = messageShema.validate({ to: to, text: text, type: type })
+    if (error) {
         return res.status(422)
     }
-    if (!(type=="private_message" || type=="message" )){
+    if (!(type == "private_message" || type == "message")) {
         return res.status(422).send("type precisa estar no formato!")
     }
-    const message={
-        from:user,
-        to:to,
-        text:text,
-        type:type,
-        time:lastStatus
+    const message = {
+        from: user,
+        to: to,
+        text: text,
+        type: type,
+        time: lastStatus
     }
 
-    try{
-       await messagesCollection.insertOne(message)
-       console.log("mensagem enviada!")
-       console.log(message)
-        return res.status(201)
-    }catch(err){
+    try {
+        await messagesCollection.insertOne(message)
+        console.log("mensagem enviada!")
+        console.log(message)
+        res.sendStatus(201)
+    } catch (err) {
         console.log(err)
-    }   
-     
+        res.sendStatus(422)
+    }
+
 })
 
-app.get("/messages",async (req,res)=>{
+app.get("/messages", async (req, res) => {
     const limit = Number(req.query.limit);
     const { user } = req.headers;
-  
+
     try {
-      const messages = await messagesCollection
-        .find({
-          $or: [
-            { from: user },
-            { to: { $in: [user, "Todos"] } }, 
-            { type: "message" },
-          ],
-        }).limit(limit).toArray();
-  
-      if (messages.length < 1) {
-        return res.status(404).send("nenhuma mensagem encontrada!");
-      }
-      res.send(messages);
+        const messages = await messagesCollection
+            .find({
+                $or: [
+                    { from: user },
+                    { to: { $in: [user, "Todos"] } },
+                    { type: "message" },
+                ],
+            }).limit(limit).toArray();
+
+        if (messages.length < 1) {
+            return res.status(404).send("nenhuma mensagem encontrada!");
+        }
+        res.send(messages);
     } catch (err) {
-      console.log(err);
-      res.status(422);
+        console.log(err);
+        res.sendStatus(422);
     }
 })
 
-app.get("/status",async (req,res)=>{
-    const {user}=req.headers
-    try{
-        const userVerify= await userCollection.findOne({name:user})
-        if (!userVerify){
-           return res.status(401).send("usuário não encontrado!")
+app.get("/status", async (req, res) => {
+    const { user } = req.headers
+    try {
+        const userVerify = await userCollection.findOne({ name: user })
+        if (!userVerify) {
+            return res.status(401).send("usuário não encontrado!")
         }
         await userCollection.updateOne(
             { name: user }, { $set: { lastStatus: Date.now() } }
-            )
-            res.sendStatus(200)
-    }catch(err){
+        )
+        res.sendStatus(200)
+    } catch (err) {
         console.log(err)
+        res.sendStatus(500);
     }
 })
-setInterval( async ()=>{
-const timesOut =Date.now() - 10000
- const toDelete=[]
-try{
-    const boxUsers= await userCollection.find().toArray()
-   
-    for(let i=0;i<boxUsers.length;i++){
-       
-        
 
-        if(boxUsers[i].lastStatus<timesOut){
-            toDelete.push(boxUsers[i].name)
+setInterval(async () => {
+
+    const timesout = Date.now() - 10000;
+
+    try {
+
+        const Inactives = await userCollection
+            .find({ lastStatus: { $lte: timesout } })
+            .toArray();
+
+        if (Inactives.length > 0) {
+
+            const inativeMessage = participantsInactives.map((participant) => {
+                return {
+                    from: participant.name,
+                    to: "Todos",
+                    text: "sai da sala...",
+                    type: "status",
+                    time: lastStatus,
+                };
+            });
+
+            await messagesCollection.insertMany(inativeMessage);
+            await userCollection.deleteMany({ lastStatus: { $lte: timesout } })
         }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
     }
-    console.log(toDelete)
-    await userCollection.find({name:toDelete})
-    await userCollection.deleteMany({name:toDelete})
-}catch(err){
-    console.log(err)
-}
+}, 15000);
 
-},15000)
-lastStatus
-
-app.listen(5000, ()=> console.log("app rodando"))
+app.listen(5000, () => console.log("app rodando"))
